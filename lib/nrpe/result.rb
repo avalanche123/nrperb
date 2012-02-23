e354a9f5664d5b3799543675d7343609c335b9fd
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-02-22 21:09:12 -0800

    simplify code

--- lib/nrpe/result.rb
+++ lib/nrpe/result.rb
@@ -1,25 +1,19 @@
 module NRPE
-  class Result
+  class Result < Struct.new(:code, :text)
     CODE_OK = 0
     CODE_WARNING = 1
     CODE_CRITICAL = 2
 
-    attr_reader :code, :text
-
-    def initialize(code, text)
-      @code, @text = code, text
-    end
-
     def ok?
-      @code == CODE_OK
+      code == CODE_OK
     end
 
     def warning?
-      @code == CODE_WARNING
+      code == CODE_WARNING
     end
 
     def critical?
-      @code == CODE_CRITICAL
+      code == CODE_CRITICAL
     end
   end
 end
